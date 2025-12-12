00573d150830a8b24100394b807fc30323379bce
Chiente Ku <chiente.ku@gmail.com>
2025-12-12 16:41:34 +0800

    finishe exercises 12

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -8,10 +8,17 @@ fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
+    // Option 1:
     match optional_point {
-        Some(p) => println!("Coordinates are {},{}", p.x, p.y),
+        Some(ref p) => println!("Coordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
+
+    // Option 2:
+    // match &optional_point {
+    //     Some(p) => println!("Coordinates are {}, {}", p.x, p.y),
+    //     _ =>panic!("No match!"),
+    // }
 
     println!("{optional_point:?}"); // Don't change this line.
 }
